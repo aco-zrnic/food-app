690d61d27b905b23211fe35a049712ed571a879e
Aleksandar Zrnic <a_zrnic@yahoo.com>
2021-08-23 21:56:21 +0200

    Error handeling

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,38 +1,43 @@
 import React ,{useState} from 'react'
-import { Text, View} from 'react-native'
+import { Text, View, Alert} from 'react-native'
 import SearchBar from './components/SearchBar'
 import yelp from '../api/yelp'
 
 
-async function SearchAPI (termInput){
-    console.log(termInput)
-    const response= await yelp.get('/search',{
-        params: {
-            limit:50,
-            term : termInput,
-            location: 'san jose'
-        }
-    });
-    return response.data.businesses
+async function SearchAPI(termInput) {
+    try {
+        const response = await yelp.get('/search', {
+            params: {
+                limit: 50,
+                term: termInput,
+                location: 'san jose'
+            }
+        });
+        return response.data.businesses
+    }
+    catch (err) {
+        console.log(err);
+        Alert.alert('Error','Something went wrong!',
+        [
+            {
+                text:'Ok',
+                onPress: ()=>console.log('Ok button is pressed')
+            }
+        ])
+    }
+    return []
 }
 
 const SearchScreen = function(props){
     const [term,setTerm] = useState('')
     const [results,setResults]=useState([])
 
-    
-
-
-
-
-
     return(
         <View>
            <SearchBar term={term} OnTermChange={(newTerm)=>{setTerm(newTerm)}}
-                onTermSubmit={()=>{
+                onTermSubmit={async ()=>{
                     console.log('termn was submited')
-                    let temp= await SearchAPI(term)
-                    setResults( temp)}}
+                    setResults(await SearchAPI(term))}}
            />
            <Text>We have found {results.length} results</Text>
         </View>)
