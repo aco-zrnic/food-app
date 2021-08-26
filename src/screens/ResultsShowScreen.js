48db02bbc0a7382042922a32459134b8c749acf1
Aleksandar Zrnic <a_zrnic@yahoo.com>
2021-08-26 22:04:58 +0200

    ResultsShowScreen changes

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React,{useEffect, useState}from 'react';
 import { Text, View, StyleSheet } from 'react-native';
+import yelp from '../api/yelp'
+
+async function SearchAPI(id) {
+    try {
+        const response = await yelp.get('/'+id);
+        return response.data;
+    }
+    catch (err) {
+        console.log(err);
+        Alert.alert('Error','Something went wrong!',
+        [
+            {
+                text:'Ok',
+                onPress: ()=>console.log('Ok button is pressed, Error happened')
+            }
+        ])
+    }
+}
 
 const ResultsShowScreen = function ({navigation}) {
     
+    const[shopInfo,setShopInfo]=useState(null)
+
     console.log(navigation.getParam('id'))
+    const id = navigation.getParam('id')
+
+    useEffect(()=>{
+        SearchAPI(id).then(response=>{setShopInfo(response)})
+    },[])
     return (
         <View>
-            <Text>Result Show</Text>
+            {shopInfo instanceof Object && <Text>{shopInfo.alias}</Text>}
         </View>
     )
 }
